750fcbfc0a9acb746322d7f47c21c47cfdf1c8ac
gabritfarias <126522875+gabritfarias@users.noreply.github.com>
2026-08-11 18:40:30 -0300

    Add files via upload
    compara qual número é maior e se houver números iguais e um diferente ou se houver todos os números iguais

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-console.log("praticando a soma de dois numeros")
+console.log("Vamos inserir e descobrir o maior número")
 
+const numero1 = parseInt(prompt("Digite o primeiro número:"));
+const numero2 = parseInt(prompt("Digite o segundo número:"));
+const numero3 = parseInt(prompt("Digite o terceiro número:"));
 
-const  nota1= parseInt(prompt("digite o numero: "))
-const  nota2= parseInt(prompt("digite o numero: "))
-const  nota3= parseInt(prompt("digite o numero: "))
-const  nota4= parseInt(prompt("digite o numero: "))
-
-
-const soma = nota1 + nota2 + nota3 + nota4
-const media = soma / 4
-console.log("A soma dos numeros é: " + soma)
-console.log("A média dos numeros é: " + media)
-
-if (media > 7){
-    console.log("Nota a cima da média")
-} else if (media === 7){
-    console.log("Na média!")
-} else {
-    console.log("Abaixo da média!")
+if (numero1 === numero2 && numero2 === numero3){
+    console.log("Todos os números são iguais: "+ numero1);
+} else if (numero1 === numero2 && numero1 > numero3){
+    console.log("O primeiro e o segundo são iguais e maiores que o terceiro: " + numero1);
+} else if (numero1 === numero3 && numero1 > numero2){
+    console.log("O primeiro e o terceiro são iguais e maiores que o segundo: " + numero1);
+} else if (numero2 === numero3 && numero2 > numero1){
+    console.log("O segundo e o terceiro são iguais e maiores que o primeiro: " + numero2);
+} else if (numero1 > numero2 && numero1 > numero3) {
+    console.log("O primeiro é o maior número: " + numero1);
+} else if (numero2 > numero1 && numero2 > numero3) {
+    console.log("O segundo é o maior número: " + numero2);
+}else {
+    console.log("O terceiro é o maior número: " + numero3);
 }
